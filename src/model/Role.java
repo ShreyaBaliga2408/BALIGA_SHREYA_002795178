2c64d51492dad16236901c712dfe58d7e5bae748
ShreyaBaliga2408 <baliga.s@northeastern.edu>
2022-10-30 18:07:29 -0400

    Commiting new files

---
+++ src/model/Role.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Shreya Baliga
+ */
+public class Role {
+    
+    private String name;
+    private int id;
+    
+    
+    
+}
